68c248191af51eb68c6abdbbee7dfb3c799a16f8
Edialvesgon <eagoncalves@minha.fag.edu.br>
2025-10-01 19:21:16 -0300

    ladingPage

---
+++ segundobi/lading-page/page-lading/src/Home.tsx
@@ -0,0 +1,16 @@
+import  './Home.css'
+import "./Utility.css"
+
+export default function Home(){
+    return(
+        <div className="container">
+            <a href="">home</a>
+            <h1 >CU</h1>
+            <button className='btn-wrapper'> teste</button>
+            <ul>
+                <li py-base>osdijf</li>
+            </ul>
+        </div>
+    )
+}
+
