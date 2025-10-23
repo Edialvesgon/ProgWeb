d3df5bec86272d76edf9ef13ff5cd8db13f68b1b
Edialvesgon <eagoncalves@minha.fag.edu.br>
2025-10-23 13:45:03 -0300

    testimoniala

--- segundobi/lading-page/page-lading/src/page/Home.tsx
+++ segundobi/lading-page/page-lading/src/page/Home.tsx
@@ -11,6 +11,7 @@ import rectagleOne from '../assets/images/retanguloMaior.svg'
 import rectagleTwo from '../assets/images/retanguloMenor.svg'
 import Card from '../components/Card'
 import "../styles/Hero.css"
+import '../styles/Testimonials.css'
 
 export default function Home() {
 
@@ -176,6 +177,97 @@ export default function Home() {
                 </section>
 
             </section>
+            {/* testimonial */}
+            <section id="testimonials">
+                <header>
+                    <span>
+                        <p className="desktop-only">
+                            Conselho de quem conhece
+                        </p>
+                        <h2>Cada cliente importa!</h2>
+                    </span>
+                    <p>
+                        Quem já pediu sabe da qualidade das nossas receitas, estamos tirando aquela ideia de que
+                        comida congelada tem de ser algo sem gosto, acompanhe abaixo os testemunhos de quem já comprou e aprovou.
+                    </p>
+                </header>
+                <section className="carousel">
+                    <div className="carousel-content">
+                        <div className="carousel-card">
+                            <img src={menu} alt="Imagem perfil cliente" />
+                            <span className="testimony">
+                                <p>
+                                    Certamente o mercado chinês de eletricos está bombando, só existe
+                                    uma coisa melhor do que isso, provar uma boa comida DonaFrost no almoço.
+                                </p>
+                            </span>
+                            <span className="rating">
+                                <img src={menu} alt="ícone estrela" width={22} height={20} />
+                                <img src={menu} alt="ícone estrela" width={22} height={20} />
+                                <img src={menu} alt="ícone estrela" width={22} height={20} />
+                                <img src={menu} alt="ícone estrela" width={22} height={20} />
+                                <img src={menu} alt="ícone estrela sem fundo" width={20} height={22} />
+                            </span>
+                            <span className="names">
+                                <p>Ellon Ma</p>
+                                <p>CEO BING CHILLING</p>
+                            </span>
+                        </div>
+
+
+                    </div>
+
+                    <div className="carousel-content">
+                        <div className="carousel-card">
+                            <img src={menu} alt="Imagem perfil cliente" />
+                            <span className="testimony">
+                                <p>
+                                    Certamente o mercado chinês de eletricos está bombando, só existe
+                                    uma coisa melhor do que isso, provar uma boa comida DonaFrost no almoço.
+                                </p>
+                            </span>
+                            <span className="rating">
+                                <img src={menu} alt="ícone estrela" width={22} height={20} />
+                                <img src={menu} alt="ícone estrela" width={22} height={20} />
+                                <img src={menu} alt="ícone estrela" width={22} height={20} />
+                                <img src={menu} alt="ícone estrela" width={22} height={20} />
+                                <img src={menu} alt="ícone estrela sem fundo" width={20} height={22} />
+                            </span>
+                            <span className="names">
+                                <p>Ellon Ma</p>
+                                <p>CEO BING CHILLING</p>
+                            </span>
+                        </div>
+
+
+                    </div>
+                    <div className="carousel-content">
+                        <div className="carousel-card">
+                            <img src={menu} alt="Imagem perfil cliente" />
+                            <span className="testimony">
+                                <p>
+                                    Certamente o mercado chinês de eletricos está bombando, só existe
+                                    uma coisa melhor do que isso, provar uma boa comida DonaFrost no almoço.
+                                </p>
+                            </span>
+                            <span className="rating">
+                                <img src={menu} alt="ícone estrela" width={22} height={20} />
+                                <img src={menu} alt="ícone estrela" width={22} height={20} />
+                                <img src={menu} alt="ícone estrela" width={22} height={20} />
+                                <img src={menu} alt="ícone estrela" width={22} height={20} />
+                                <img src={menu} alt="ícone estrela sem fundo" width={20} height={22} />
+                            </span>
+                            <span className="names">
+                                <p>Ellon Ma</p>
+                                <p>CEO BING CHILLING</p>
+                            </span>
+                        </div>
+
+
+                    </div>
+                </section>
+            </section>
+
         </>
     )
 }
