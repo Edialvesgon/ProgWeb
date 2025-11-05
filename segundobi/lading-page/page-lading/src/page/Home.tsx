be38182a03d413a6bf1a06a3bacfad3b59ba84aa
Edialvesgon <eagoncalves@minha.fag.edu.br>
2025-11-05 19:43:53 -0300

    pricing

--- segundobi/lading-page/page-lading/src/page/Home.tsx
+++ segundobi/lading-page/page-lading/src/page/Home.tsx
@@ -2,6 +2,7 @@ import '../styles/Utility.css'
 import '../styles/Home.css'
 import '../styles/header.css'
 import '../styles/Solution.css'
+import '../styles/Pricing.css'
 import logo from '../assets/Untitled (2).svg'
 import close from '../assets/close_24dp_1F1F1F_FILL0_wght400_GRAD0_opsz24.svg'
 import menu from '../assets/menu_24dp_1F1F1F_FILL0_wght400_GRAD0_opsz24.svg'
@@ -264,6 +265,84 @@ export default function Home() {
                         </div>
 
 
+                    </div>
+                </section>
+
+                {/* princing */}
+            </section>
+
+            <section id="pricing" className="container">
+                <header>
+                    <p className="desktop-only">Planos e preços</p>
+                    <h2>Nossos planos</h2>
+                </header>
+                <section className="even-columns gap-1.5">
+
+                    <div className="pricing-card">
+                        <span className="plan">
+                            <h3>Básico</h3>
+                            <p>Você tem direito a uma prova das comidas DonaFrost.</p>
+                        </span>
+                        <h2>Grátis</h2>
+                        <Button text="Pedir agora" secondary key="free" />
+                        <span className="hr" /><span className="features">
+                            <img src={menu} alt="ícone check" width={24} height={24} />
+                            <p>Retire na loja</p>
+                        </span>
+                        <ul className="features">
+                            <li>
+                                <img src={menu} alt="ícone check" width={24} height={24} />
+                                <p>Apenas 1 por CPF</p>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div className="pricing-card premium">
+                        <span className="bonus">
+                            <p>1º MÊS COM DESCONTO</p>
+                        </span>
+                        <span className="plan">
+                            <h3>Premium</h3>
+                            <p>Para quem precisa de uma marmita diária, muito saborosa.</p>
+                        </span>
+                        <span className="price">
+                            <h2>R$ 89,90</h2>
+                            <p>/mês</p>
+                        </span>
+                        <Button text="Pedir agora" key="premium" />
+                        <span className="hr" />
+                        <span className="features">
+                            <img src={menu} alt="ícone check" width={24} height={24} />
+                            <p>2 Entregas</p>
+                        </span>
+                        <span className="features">
+                            <img src={menu} alt="ícone check" width={24} height={24} />
+                            <p>5 Refeições por semana</p>
+                        </span>
+                        <span className="features">
+                            <img src={menu} alt="ícone check" width={24} height={24} />
+                            <p>2 Sucos por semana</p>
+                        </span>
+                    </div>
+
+
+                    <div className="pricing-card">
+                        <span className="plan">
+                            <h3>Básico</h3>
+                            <p>Você tem direito a uma prova das comidas DonaFrost.</p>
+                        </span>
+                        <h2>Grátis</h2>
+                        <Button text="Pedir agora" secondary key="free" />
+                        <span className="hr" /><span className="features">
+                            <img src={menu} alt="ícone check" width={24} height={24} />
+                            <p>Retire na loja</p>
+                        </span>
+                        <ul className="features">
+                            <li>
+                                <img src={menu} alt="ícone check" width={24} height={24} />
+                                <p>Apenas 1 por CPF</p>
+                            </li>
+                        </ul>
                     </div>
                 </section>
             </section>
